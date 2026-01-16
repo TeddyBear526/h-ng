a9d8c54e9a0da3ec9299f5e8a5e343d2f830e9d4
TeddyBear526 <theodor.bygdemark@elev.ga.ntig.se>
2026-01-16 09:39:11 +0100

    j

--- häng.py
+++ häng.py
@@ -1,12 +1,20 @@
-word = "test" 
+import random
+ord = ["__","__","__","__"]
+p = ["fest", "pest", "mest", "best", "cest"]
+
+print(p[2])
+
+print(list(p[2]))
 
 g = 8
 gl = 0
 guessed_letters = []
 while g > gl:
+    word = random.choice(p)
     a = str(input("gissa en bokstav "))
     if a in guessed_letters:
         print("Du har redan gissat den bokstaven.")
         continue
     guessed_letters.append(a)
-    input("hej ")
+    if a in word:
+        print("Correct gisning")
