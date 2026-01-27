fb277e74369fe99c8847519b7f24acb4f721ff3b
TeddyBear526 <theodor.bygdemark@elev.ga.ntig.se>
2026-01-27 11:58:48 +0100

    Changed some of the variables to make to code more readable, i also put in so the code says what letters you've guessed and how many guesses you have left

--- häng.py
+++ häng.py
@@ -2,30 +2,32 @@ import random
 ord = ["galaxy", "bucket", "whisper", "velvet", "cactus", "journey", "puzzle", "echo", "lantern", "orbit", "breeze", "mosaic", "anchor", "lemon", "guitar", "fossil", "shadow", "rocket", "pillow", "canyon", "marble", "fabric", "drift", "ember", "jungle", "novel", "quilt", "harbor", "insect", "wagon", "yield", "kite", "ocean", "xenon", "frost", "glitch", "meadow", "nickel", "umbrella", "violin", "waffle", "yacht", "zebra", "alarm", "bridge", "camera", "doctor", "eagle", "flame", "grape"]
 word = random.choice(ord)
 blank = ["_"]*len(word)
-g = 8
-gl = 0
-rätt = 0
+guesses = 8
+guesses_left = 0
+right = 0
 guessed_letters = []
 
-while g > gl:
+while guesses > guesses_left:
     correct_plats = 0
-    a = str(input("gissa en bokstav "))
-    if a in guessed_letters:
+    guessed = str(input("gissa en bokstav "))
+    if guessed in guessed_letters:
         print("Du har redan gissat den bokstaven.")
+        print(f"Du har gissat", guessed_letters)
         continue
-    guessed_letters.append(a)
-    if a in word:
+    guessed_letters.append(guessed)
+    if guessed in word:
         print(f"Correct gisning, Bokstaven är på plats")
         for i, letter in enumerate(word):
-            if letter == a:
-                blank[i] = a
+            if letter == guessed:
+                blank[i] = guessed
                 correct_plats += 1
         print(blank)
-        rätt += correct_plats
+        right += correct_plats
     else:
         print("bokstaven är inte i ordet")
-        g+= 1
-    if rätt == len(word) :
+        guesses += 1; print(f"Du har {guesses_left} Gissningar kvar")
+        print(f"Du har gissat", guessed_letters)
+    if right == len(word) :
         print("Hela ordet gissat")
         print(f"ordet var", word)
         break
